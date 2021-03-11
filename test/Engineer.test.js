96c4058980d884a7dfede4cf9b02911bb80492c2
Andrew Coon <thenran@gmail.com>
2021-03-10 19:29:52 -0500

    Written tests for Engineer class. Corrected typo in Engineer getGithub method.

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -1,15 +1,30 @@
 // Starter file provided by Instructor (03/09/2021) AC
 
+const { expect } = require("@jest/globals");
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
+  const name = "Andrew";
+  const id = 0;
+  const email = "123@test.com";
+  const github = "test";
+  const obj = new Engineer(name, id, email, github);
 
+  expect(obj.github).toEqual(github);
 });
 
 test("getRole() should return \"Engineer\"", () => {
+  const obj = new Engineer();
 
+  expect(obj.getRole()).toEqual("Engineer");
 });
 
 test("Can get GitHub username via getGithub()", () => {
+  const name = "Andrew";
+  const id = 0;
+  const email = "123@test.com";
+  const github = "test";
+  const obj = new Engineer(name, id, email, github);
 
+  expect(obj.getGithub()).toEqual(github);
 });

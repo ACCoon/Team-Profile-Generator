d99329d1339e533ab9e27e54dbf66858eeb5d9b5
Andrew Coon <thenran@gmail.com>
2021-03-10 19:35:45 -0500

    Written tests for Intern class.

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,15 +1,24 @@
 // Starter file provided by Instructor (03/09/2021) AC
 
+const { expect } = require("@jest/globals");
 const Intern = require("../lib/Intern");
 
 test("Can set school via constructor", () => {
+  const school = "UNCC";
+  const obj = new Intern("", 0, "", school);
 
+  expect(obj.school).toEqual(school);
 });
 
 test("getRole() should return \"Intern\"", () => {
+  const obj = new Intern();
 
+  expect(obj.getRole()).toEqual("Intern");
 });
 
 test("Can get school via getSchool()", () => {
+  const school = "UNCC";
+  const obj = new Intern("", 0, "", school);
 
+  expect(obj.getSchool()).toEqual(school);
 });
